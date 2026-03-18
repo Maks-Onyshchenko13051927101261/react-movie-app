fcf266ebfd9dbd55b2405a5425264292da0786d8
Maks Onyshchenko <onyshchenkomaksymm@gmail.com>
2026-03-18 04:44:32 +0000

    created project structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,12 @@
-import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import './index.css'
+import {Provider} from "react-redux";
+import {RouterProvider} from "react-router-dom";
+import {routes} from "./router/routes";
+import {store} from "./redux/store/store";
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-
-  </StrictMode>,
+    <Provider store={store}>
+        <RouterProvider router={routes}/>
+    </Provider>,
 )
